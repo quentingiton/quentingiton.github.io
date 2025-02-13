1ed72c76104ceeac01b503bbfecee39b4e63a470
Camille Jouan <camille.pauron@gmail.com>
2025-02-13 13:53:34 +0100

    better colors

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <header>
     <nav id="desktop-nav">
-      <div class="logo">Quentin Giton</div>
-      <div>
-        <ul class="nav-links">
-          <li><a href="/#teaching">Teaching</a></li>
-          <li><a href="/#publications">Publications</a></li>
-          <li><a href="/#conferences">Conferences</a></li>
-          <li>
-            <RouterLink to="/"
-              ><i class="fas fa-house" style="color: #000000"></i
-            ></RouterLink>
-          </li>
-        </ul>
+      <div class="logo"><RouterLink to="/">Quentin Giton</RouterLink></div>
+      <div class="nav-links">
+        <RouterLink to="/teaching" active-class="link-active"
+          >Teaching</RouterLink
+        >
+        <RouterLink to="/publications" active-class="link-active"
+          >Publications</RouterLink
+        >
+        <RouterLink to="/"><i class="fas fa-house"></i></RouterLink>
       </div>
     </nav>
   </header>
